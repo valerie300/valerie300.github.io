f20ed5d228e95d9c3894b2d925306f28d8331019
valerie300 <zxcv841219595@gmail.com>
2021-07-07 20:42:40 +0800

    git init

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
  
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f https://github.com/valerie300/sixweek-6.git master:gh-pages
+git push -f https://github.com/valerie300/valerie300.github.io.git main:gh-pages
  
 cd -
