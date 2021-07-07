d4477781efa662afe42b70a04cd6a8aeaadba8f2
valerie300 <zxcv841219595@gmail.com>
2021-07-07 19:57:11 +0800

    git init

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/valerie300/sixweek-6/tree/gh-pages/dist/' : './',
+  publicPath: process.env.NODE_ENV === 'production' ? '/repoName/' : '/',
 };
